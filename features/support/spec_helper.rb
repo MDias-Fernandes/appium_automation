d2fdb01ba9c1aa45ab821bd76d5bde1ce12586d8
MDias-Fernandes <diasmatehus621@gmail.com>
2018-07-12 22:31:13 -0300

    20180712 - 22:31

--- features/support/spec_helper.rb
+++ features/support/spec_helper.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 # !/usr/bin/env ruby
-Dir[File.join(File.dirname(__FILE__), '../core_app_test/screens/*.rb')].each { |file| require file }
+Dir[File.join(File.dirname(__FILE__), '../screens/*.rb')].each { |file| require file }
 
 module Screen
     def common_screen
